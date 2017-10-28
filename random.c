7950f09ec5b6bcdf3e588ae1e09aa9444fa2102c
JFreud <jrm.frdnbrg@gmail.com>
2017-10-27 20:57:18 -0400

    doesnt work

--- random.c
+++ random.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 
-unsigned int genRan() {
+unsigned int genRan() {//generate random unsigned int from /dev/random
 
   int fd = open("/dev/random", O_RDONLY);
 
@@ -16,31 +16,56 @@ unsigned int genRan() {
 
   read(fd, buffer, sizeof(buffer));
 
-  return *buffer;
-}
+  unsigned int val = *buffer;
 
+  free(buffer);
+
+  return val;
+}
 
 
 
 
 int main() {
 
-  printf("Generating random numbers:\n");
+  printf("\nGenerating random numbers:\n");
 
-  unsigned int randarr[10];
+  unsigned int randarr[10];//create array
 
   int i;
   for (i = 0; i < 10; i++){
-    randarr[i] = genRan();
-  }
-
-  int j;
-  for (j = 0; j < 10; j++) {
-    printf("random %d: %u\n", j, randarr[j]);
+    randarr[i] = genRan();//populate array
   }
 
 
+  for (i = 0; i < 10; i++) {
+    printf("random %d: %u\n", i, randarr[i]);//print array
+  }
 
+  //write array to file:
+
+  printf("\nWriting numbers to file...\n");
+
+  int fd_write = open("random_nums", O_CREAT, 0644);//create file to write to
+
+  write(fd_write, randarr, sizeof(randarr));
+
+  close(fd_write);
+
+  //read file to array:
+
+  printf("\nReading numbers from file...\n\n");
+
+  int fd_read = open("random_nums", O_RDONLY);//open file that was written
+
+  unsigned int randarr_check[10];
+
+  read(fd_read, randarr_check, sizeof(randarr_check));//read to new array
+
+
+  for (i = 0; i < 10; i++){
+    printf("random %d: %u\n", i, randarr_check[i]);//print array
+  }
 
   return 0;
 }
